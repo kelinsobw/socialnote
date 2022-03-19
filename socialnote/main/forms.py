1c302ae8f06b842f8d8a08fee49b95a944e9fa58
Siarhei Harelik <kelinso@mail.ru>
2022-03-19 12:51:41 +0300

    Add form add table

--- socialnote/main/forms.py
+++ socialnote/main/forms.py
@@ -1,7 +1,32 @@
 from django import forms
 
 
-'''class AddDatabase(forms.Form):
-    post_formset = modelformset_factory(Post, fields=('firstname', 'lastname', 'adult'))
+class AddDatabase(forms.Form):
+    type_choice = [
+        ("Ch", 'Ch'),
+        ("F", 'Female')]
+    privates = [
+        ("None", "None"),
+        ("Friends", "Friends"),
+        ("Absolutely", "Absolutely")
+    ]
+    table_name = forms.CharField()
+    table_description = forms.CharField()
+    table_privates = forms.ChoiceField(choices=privates)
+    base_1 = forms.CharField()
+    type_1 = forms.ChoiceField(choices=type_choice)
+    base_2 = forms.CharField(required=False)
+    type_2 = forms.ChoiceField(choices=type_choice)
+    base_3 = forms.CharField(required=False)
+    type_3 = forms.ChoiceField(choices=type_choice)
+    base_4 = forms.CharField(required=False)
+    type_4 = forms.ChoiceField(choices=type_choice)
+    base_5 = forms.CharField(required=False)
+    type_5 = forms.ChoiceField(choices=type_choice)
+    base_6 = forms.CharField(required=False)
+    type_6 = forms.ChoiceField(choices=type_choice)
+    base_7 = forms.CharField(required=False)
+    type_7 = forms.ChoiceField(choices=type_choice)
+    base_8 = forms.CharField(required=False)
+    type_8 = forms.ChoiceField(choices=type_choice)
 
-'''
--- socialnote/main/views.py
+++ socialnote/main/views.py
@@ -1,11 +1,12 @@
 import logging
 
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
-from main.forms import AddDatabase
+from main.forms import  AddDatabase
 
 logger = logging.getLogger(__name__)
+
 
 def index(request):
     return render(request, "main/register.html")
@@ -16,9 +17,10 @@ def add_database(request):
         if request.method == "POST":
             form = AddDatabase(request.POST, request.FILES)
             if form.is_valid():
-                print("0000000000000000000000000000000000000000")
-                return redirect("main/home")
+                info_base = form.cleaned_data
+                print(info_base)
+                return HttpResponse("You don't authenticated!")
         else:
             form = AddDatabase()
-        return render(request, "main/home.html", {"form": form})
+        return render(request, "main/add_base.html", {"form": form})
     return HttpResponse("You don't authenticated!")
